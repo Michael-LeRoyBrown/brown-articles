f32ed6a6ec000956e87d217b3fb71cf375765a26
kraziibone <michaelgreen9812@yahoo.com>
2022-02-06 14:06:22 -0600

    changes for heroku

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -14,7 +14,6 @@ import os
 from pathlib import Path
 import django_heroku
 from decouple import config
-from secret_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
